e52fe5695b2a80ad514583f88d53f69fd4aedb5c
Gustavo Maia <ms.gustavo@outlook.com>
2024-09-03 16:35:36 -0300

    alteracoes com valores propostos pelo desafio

--- faturamentoDiario.js
+++ faturamentoDiario.js
@@ -1,34 +1,34 @@
 const dailyBilling = [
-  { day: 1, value: 1500 },
-  { day: 2, value: 1700 },
-  { day: 3, value: 2000 },
-  { day: 4, value: 1900 },
-  { day: 5, value: 1800 },
-  { day: 6, value: 2100 },
-  { day: 7, value: 2200 },
-  { day: 8, value: 2300 },
-  { day: 9, value: 2400 },
-  { day: 10, value: 2500 },
-  { day: 11, value: 0 },
-  { day: 12, value: 2700 },
-  { day: 13, value: 2800 },
-  { day: 14, value: 0 },
-  { day: 15, value: 3000 },
-  { day: 16, value: 3100 },
-  { day: 17, value: 0 },
-  { day: 18, value: 3300 },
-  { day: 19, value: 0 },
-  { day: 20, value: 3500 },
-  { day: 21, value: 3600 },
-  { day: 22, value: 0 },
-  { day: 23, value: 3800 },
-  { day: 24, value: 0 },
-  { day: 25, value: 4000 },
-  { day: 26, value: 0 },
-  { day: 27, value: 0 },
-  { day: 28, value: 4300 },
-  { day: 29, value: 0 },
-  { day: 30, value: 4500 },
+  { day: 1, value: 22174.1664 },
+  { day: 2, value: 24537.6698 },
+  { day: 3, value: 26139.6134 },
+  { day: 4, value: 0.0 },
+  { day: 5, value: 0.0 },
+  { day: 6, value: 26742.6612 },
+  { day: 7, value: 0.0 },
+  { day: 8, value: 42889.2258 },
+  { day: 9, value: 46251.174 },
+  { day: 10, value: 11191.4722 },
+  { day: 11, value: 0.0 },
+  { day: 12, value: 0.0 },
+  { day: 13, value: 3847.4823 },
+  { day: 14, value: 373.7838 },
+  { day: 15, value: 2659.7563 },
+  { day: 16, value: 48924.2448 },
+  { day: 17, value: 18419.2614 },
+  { day: 18, value: 0.0 },
+  { day: 19, value: 0.0 },
+  { day: 20, value: 35240.1826 },
+  { day: 21, value: 43829.1667 },
+  { day: 22, value: 18235.6852 },
+  { day: 23, value: 4355.0662 },
+  { day: 24, value: 13327.1025 },
+  { day: 25, value: 0.0 },
+  { day: 26, value: 0.0 },
+  { day: 27, value: 25681.8318 },
+  { day: 28, value: 1718.1221 },
+  { day: 29, value: 13220.495 },
+  { day: 30, value: 8414.61 },
 ];
 
 function calcBill(dailyBilling) {
@@ -52,7 +52,15 @@ function calcBill(dailyBilling) {
 
 const results = calcBill(dailyBilling);
 console.group("Resultados");
-console.log(`Menor valor: R$ ${results.lowerValue}`);
-console.log(`Maior valor: R$ ${results.higherValue}`);
+console.log(
+  `Menor valor: R$ ${results.lowerValue.toLocaleString("pt-BR", {
+    minimumFractionDigits: 2,
+  })}`
+);
+console.log(
+  `Maior valor: R$ ${results.higherValue.toLocaleString("pt-BR", {
+    minimumFractionDigits: 2,
+  })}`
+);
 console.log(`Dias acima da média: ${results.daysAboveAverage}`);
 console.groupEnd();
